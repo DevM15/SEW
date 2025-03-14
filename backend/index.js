723b2887041be6ecf297f1d78962c952b4dcb7d2
unknown <devmistry15@gmail.com>
2025-03-14 11:38:39 +0530

    Fixed fetch link

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const multer = require("multer");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const { MongoClient, GridFSBucket, ObjectId } = require("mongodb");
 
